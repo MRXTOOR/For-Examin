54dcfd09d0bf84a85ec74bb783becee7daeb3af8
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-07-02 01:48:22 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.jobAgencyDataSet = new WindowsFormsApp1.JobAgencyDataSet();
+            this.jobsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.jobsTableAdapter = new WindowsFormsApp1.JobAgencyDataSetTableAdapters.JobsTableAdapter();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.comboType = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtSearchTitle = new System.Windows.Forms.TextBox();
+            this.txtSearchSalary = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.carsDataSet = new WindowsFormsApp1.carsDataSet();
+            this.carsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.carsTableAdapter = new WindowsFormsApp1.carsDataSetTableAdapters.CarsTableAdapter();
+            this.carCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.brandNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.manufactureYearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobAgencyDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.carCodeDataGridViewTextBoxColumn,
+            this.brandNameDataGridViewTextBoxColumn,
+            this.colorDataGridViewTextBoxColumn,
+            this.manufactureYearDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.carsBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(12, 60);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(445, 287);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // jobAgencyDataSet
+            // 
+            this.jobAgencyDataSet.DataSetName = "JobAgencyDataSet";
+            this.jobAgencyDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // jobsBindingSource
+            // 
+            this.jobsBindingSource.DataMember = "Jobs";
+            this.jobsBindingSource.DataSource = this.jobAgencyDataSet;
+            // 
+            // jobsTableAdapter
+            // 
+            this.jobsTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(704, 283);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(704, 347);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(704, 410);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Обновить";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(485, 60);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(100, 20);
+            this.txtCode.TabIndex = 4;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(485, 102);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 5;
+            // 
+            // comboType
+            // 
+            this.comboType.Location = new System.Drawing.Point(485, 167);
+            this.comboType.Name = "comboType";
+            this.comboType.Size = new System.Drawing.Size(100, 20);
+            this.comboType.TabIndex = 6;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(485, 222);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtSalary.TabIndex = 7;
+            // 
+            // txtSearchTitle
+            // 
+            this.txtSearchTitle.Location = new System.Drawing.Point(12, 12);
+            this.txtSearchTitle.Name = "txtSearchTitle";
+            this.txtSearchTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtSearchTitle.TabIndex = 8;
+            // 
+            // txtSearchSalary
+            // 
+            this.txtSearchSalary.Location = new System.Drawing.Point(118, 12);
+            this.txtSearchSalary.Name = "txtSearchSalary";
+            this.txtSearchSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtSearchSalary.TabIndex = 9;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(225, 8);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "поиск";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // carsDataSet
+            // 
+            this.carsDataSet.DataSetName = "carsDataSet";
+            this.carsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // carsBindingSource
+            // 
+            this.carsBindingSource.DataMember = "Cars";
+            this.carsBindingSource.DataSource = this.carsDataSet;
+            // 
+            // carsTableAdapter
+            // 
+            this.carsTableAdapter.ClearBeforeFill = true;
+            // 
+            // carCodeDataGridViewTextBoxColumn
+            // 
+            this.carCodeDataGridViewTextBoxColumn.DataPropertyName = "CarCode";
+            this.carCodeDataGridViewTextBoxColumn.HeaderText = "CarCode";
+            this.carCodeDataGridViewTextBoxColumn.Name = "carCodeDataGridViewTextBoxColumn";
+            // 
+            // brandNameDataGridViewTextBoxColumn
+            // 
+            this.brandNameDataGridViewTextBoxColumn.DataPropertyName = "BrandName";
+            this.brandNameDataGridViewTextBoxColumn.HeaderText = "BrandName";
+            this.brandNameDataGridViewTextBoxColumn.Name = "brandNameDataGridViewTextBoxColumn";
+            // 
+            // colorDataGridViewTextBoxColumn
+            // 
+            this.colorDataGridViewTextBoxColumn.DataPropertyName = "Color";
+            this.colorDataGridViewTextBoxColumn.HeaderText = "Color";
+            this.colorDataGridViewTextBoxColumn.Name = "colorDataGridViewTextBoxColumn";
+            // 
+            // manufactureYearDataGridViewTextBoxColumn
+            // 
+            this.manufactureYearDataGridViewTextBoxColumn.DataPropertyName = "ManufactureYear";
+            this.manufactureYearDataGridViewTextBoxColumn.HeaderText = "ManufactureYear";
+            this.manufactureYearDataGridViewTextBoxColumn.Name = "manufactureYearDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 496);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchSalary);
+            this.Controls.Add(this.txtSearchTitle);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.comboType);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobAgencyDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private JobAgencyDataSet jobAgencyDataSet;
+        private System.Windows.Forms.BindingSource jobsBindingSource;
+        private JobAgencyDataSetTableAdapters.JobsTableAdapter jobsTableAdapter;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox comboType;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtSearchTitle;
+        private System.Windows.Forms.TextBox txtSearchSalary;
+        private System.Windows.Forms.Button btnSearch;
+        private carsDataSet carsDataSet;
+        private System.Windows.Forms.BindingSource carsBindingSource;
+        private carsDataSetTableAdapters.CarsTableAdapter carsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn carCodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn brandNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn manufactureYearDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        private SqlDataAdapter adapter;
+        private DataTable dataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Инициализация подключения к базе данных
+            string connectionString = @"Server = DESKTOP-2MGOE61; Database = cars; Integrated Security = True";
+            connection = new SqlConnection(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "carsDataSet.Cars". При необходимости она может быть перемещена или удалена.
+            this.carsTableAdapter.Fill(this.carsDataSet.Cars);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "jobAgencyDataSet.Jobs". При необходимости она может быть перемещена или удалена.
+            this.jobsTableAdapter.Fill(this.jobAgencyDataSet.Jobs);
+            // Загрузка данных с базы данных и отображение их в DataGridView
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string query = "SELECT * FROM Cars";
+            adapter = new SqlDataAdapter(query, connection);
+
+            // Создание команды вставки
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.InsertCommand = builder.GetInsertCommand();
+
+            dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView.DataSource = dataTable;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            // Обновление данных на форме из базы данных
+            LoadData();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Добавление новой записи в DataGridView и базу данных
+            // Проверка наличия записи с таким же значением в колонке VacancyCode
+            int vacancyCode;
+            if (!Int32.TryParse(txtCode.Text, out vacancyCode))
+            {
+                MessageBox.Show("Введите корректное значение для CarCode.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            bool vacancyExists = dataTable.AsEnumerable().Any(row => row.Field<int>("CarCode") == vacancyCode);
+            if (vacancyExists)
+            {
+                MessageBox.Show("Такая вакансия уже существует.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DataRow newRow = dataTable.NewRow();
+            newRow["CarCode"] = vacancyCode;
+            newRow["BrandName"] = txtTitle.Text;
+            newRow["Color"] = comboType.Text;
+            newRow["ManufactureYear"] = decimal.Parse(txtSalary.Text);
+            dataTable.Rows.Add(newRow);
+            adapter.Update(dataTable);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Удаление записи из DataGridView и базы данных
+            int vacancyCodeToDelete;
+            if (!int.TryParse(txtCode.Text, out vacancyCodeToDelete))
+            {
+                MessageBox.Show("Введите корректное значение для CarCode.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var matchingRows = dataTable.AsEnumerable().Where(row => row.Field<int>("CarCode") == vacancyCodeToDelete).ToList();
+            if (matchingRows.Count == 0)
+            {
+                MessageBox.Show("Запись с таким CarCode не найдена.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (MessageBox.Show("Вы уверены, что хотите удалить выбранную запись?", "Подтверждение удаления", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                foreach (var row in matchingRows)
+                {
+                    dataTable.Rows.Remove(row);
+                }
+
+                adapter.Update(dataTable);
+                MessageBox.Show("Запись успешно удалена.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchTitle = txtSearchTitle.Text;
+            string searchSalary = txtSearchSalary.Text;
+
+            if (searchTitle == "" && searchSalary == "")
+            {
+                MessageBox.Show("Введите значение для поиска.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DataTable filteredDataTable = dataTable.Clone(); // Создаем копию таблицы с теми же столбцами
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string vacancyTitle = row.Field<string>("BrandName");
+            
+                int salary = row.Field<int>("ManufactureYear");
+
+                if ((searchTitle == "" || vacancyTitle.IndexOf(searchTitle, StringComparison.OrdinalIgnoreCase) >= 0) &&
+                    (searchSalary == "" || int.TryParse(searchSalary, out int searchSalaryValue) && salary >= searchSalaryValue))
+                {
+                    filteredDataTable.ImportRow(row);
+                }
+            }
+
+            dataGridView.DataSource = filteredDataTable;
+        }
+    }
+}
